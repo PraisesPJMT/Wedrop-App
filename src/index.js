d1509f8035494f187f714366817af4591b136476
PraisesPJMT <praisesmusa@outlook.com>
2022-10-03 09:18:27 +0100

    Setup store for App
    - Set up weather store for app
    - Removed boiler plates

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import store from './redux/configureStore';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const AppRoot = createRoot(container);
 
-root.render(
+AppRoot.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
