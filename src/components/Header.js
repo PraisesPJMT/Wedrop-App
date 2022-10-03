ab1e6f76829e4bf4de5c7c6f4e60c29433caf0e1
PraisesPJMT <praisesmusa@outlook.com>
2022-10-03 13:05:04 +0100

    Add header search bar for city search

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,18 @@ const Header = () => {
         />
         <span className="capitalize text-xm font-bold">{ logoName }</span>
       </div>
+      <div className="input-group relative flex w-2/5 flex-wrap items-stretch shadow-md">
+        <input type="search"
+               className="form-control relative flex-auto w-4/5 block px-1 py-0.1 text-steel-blue text-bold text-xs font-normal bg-white bg-clip-padding border-white border-2 rounded-l-full transition ease-in-out focus:text-steel-blue focus:bg-white focus:border-white focus:outline-none"
+               placeholder="Search City..." aria-label="Search" aria-describedby="button-addon2"/>
+        <button
+          className="btn inline-block bg-steel-blue w-1/5 text-white font-medium text-xs leading-tight uppercase rounded-r shadow-md border-white border-2 hover:shadow-lg focus:bg-steel-blue  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+          type="button" id="button-addon2">
+          <FontAwesomeIcon
+            className="text-xs hover:scale-125 px-1.5 py-1.5"
+            icon={faMagnifyingGlassLocation} />
+        </button>
+      </div>
     </header>
   );
 };
