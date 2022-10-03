2c8065ecb154b1449955dd3ea680674eae40fced
PraisesPJMT <praisesmusa@outlook.com>
2022-10-03 07:55:37 +0100

    Setup app with TailwindCSS
    - Set up TailwindCSS dependencies
    - Add TailwindCSS directives to index.css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-cyan-200">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
