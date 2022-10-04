fa94668a6af8eaa2a49b762cec00aecdf26d9c2a
PraisesPJMT <praisesmusa@outlook.com>
2022-10-04 14:34:00 +0100

    Add Continent routing to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import Continent from './components/Continent';
 
 const App = () => (
   <>
@@ -10,6 +11,7 @@ const App = () => (
       <main className="min-h-[94vh] bg-cornflower-blue">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/:continent" element={<Continent />} />
         </Routes>
       </main>
     </Router>
