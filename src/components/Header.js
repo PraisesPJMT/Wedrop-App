81a5b4238f22fe7469c0d97e985378eeffc584fc
PraisesPJMT <praisesmusa@outlook.com>
2022-10-06 17:49:50 +0100

    Implement city weather serch functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCloudSunRain, faMagnifyingGlassLocation, faBars, faClose,
 } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { allStatus, allWeather, getWeather } from '../redux/weather/weather';
+import api from '../redux/api/api';
 
 const Header = () => {
   const [menuOpen, setMenu] = useState(false);
+  const status = useSelector(allStatus);
+  const weather = useSelector(allWeather);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const navs = [
     { name: 'Home', id: 1, path: '/' },
     { name: 'Contact', id: 2, path: '/contact' },
@@ -18,6 +25,19 @@ const Header = () => {
 
   const closeMobileMenu = () => {
     setMenu(() => false);
+  };
+
+  const navigateToCity = () => {
+    closeMobileMenu();
+    const search = document.querySelector('input[name="search"]');
+    if (search.value.length > 2) {
+      dispatch(getWeather(search.value));
+      if (status === 'succeeded') {
+        const { ContinentName, CountryName } = api.getContinentCountryName(weather.countryCode);
+        navigate(`/${ContinentName.toLowerCase().replace(/ /g, '-').replace(/'/g, '')}/${CountryName.toLowerCase().replace(/ /g, '-')}/${weather.cityName.toLowerCase().replace(/ /g, '-')}`);
+        search.value = '';
+      }
+    }
   };
 
   return (
@@ -57,16 +77,20 @@ const Header = () => {
       <div className="input-group relative flex w-2/5 flex-wrap items-stretch shadow-md z-[100]">
         <input
           type="search"
+          name="search"
           className="form-control relative flex-auto w-4/5 block px-1 py-0.1 text-steel-blue text-bold text-xs font-normal bg-white bg-clip-padding border-white border-2 rounded-l-full transition ease-in-out focus:text-steel-blue focus:bg-white focus:border-white focus:outline-none md:w-auto md:text-xl md:focus:text-xl md:pl-2.5"
           placeholder="Search City..."
           aria-label="Search"
           aria-describedby="button-addon2"
+          onKeyPress={(event) => {
+            if (event.key === 'Enter') navigateToCity();
+          }}
         />
         <button
           className="btn inline-block bg-steel-blue w-1/5 md:w-auto text-white font-medium text-xs leading-tight uppercase rounded-r shadow-md border-white border-2 hover:shadow-lg focus:bg-steel-blue  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg active:text-steel-blue active:text-bold transition duration-150 ease-in-out flex items-center justify-center md:px-2 md:py-0.5"
           type="button"
           id="button-addon2"
-          onClick={closeMobileMenu}
+          onClick={() => navigateToCity()}
         >
           <FontAwesomeIcon
             className="text-xs hover:scale-125 px-1.5 py-1.5 md:text-xl md:p-auto"
